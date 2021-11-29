33c395a773bae6da59933af4465b1c98e4beff6c
jeonyeohun <21500630@handong.edu>
2021-11-30 00:09:39 +0900

    Update InvitationViewController.swift

---
+++ MateRunner/MateRunnerViewModelTests/HomeScene/actionTest.swift
@@ -0,0 +1,15 @@
+//
+//  actionTest.swift
+//  MateRunnerViewModelTests
+//
+//  Created by 전여훈 on 2021/11/30.
+//
+
+import XCTest
+
+class DistanceSettingViewModelTests: XCTestCase {
+    func test_설정버튼탭_소수점기호로_끝나면_0두개_붙이기() {
+        let a = 1
+        XCTAssert(a == 1)
+    }
+}
